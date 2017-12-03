3766b0a25f87bd82e7f2d0afcf17f7922e877032
vritant24 <vritoku@gmail.com>
2017-12-02 23:52:22 -0500

    landing is responsive

--- src/Pages/landing.js
+++ src/Pages/landing.js
@@ -1,5 +1,6 @@
 import React    from 'react';
 import styled   from 'styled-components';
+import _        from '../helpers.js';
 
 export default function(props) {
     return (
@@ -7,7 +8,7 @@ export default function(props) {
             <MastHead>
                 <Logo/>
                 <Mission/>
-            </MastHead>
+            </MastHead> 
             <Video/>
         </Container>
     );
@@ -23,28 +24,80 @@ const Container = styled.div`
 
 const MastHead = styled.div`
     display: grid;
-    grid-template-columns: auto 200px 500px auto;
-    grid-template-rows: 200px;
+    grid-template-columns: auto 90vw auto;
+    grid-template-rows: 200px 300px;
+    align-items: center;
+    justify-content: center;
+
     align-self: center;
-    margin: 100px auto 25px;
+    margin: 100px auto 10px;
+
+    ${_.media.minTablet`
+        grid-template-columns: auto 200px 500px auto;
+        grid-template-rows: 200px;
+        align-self: center;
+        margin: 100px auto 25px;
+    `}
 `;
 
 const Logo = styled.div`
     background: #333;
+
+    width: 200px;
+    height: 200px;
+
     grid-column-start: 2;
     grid-column-end: 3;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    justify-self: center;
+
+    ${_.media.minTablet`
+        grid-column-start: 2;
+        grid-column-end: 3;
+        justify-self: stretch;
+    `}
 `;
 
 const Mission = styled.div`
     background: #777;
-    grid-column-start: 3;
-    grid-column-end: 4;
+    
+    justify-self: center;
+    align-self: center;
+
+    grid-column-start: 2;
+    grid-column-end: 3;
+    grid-row-start: 2;
+    grid-row-end: 3;
+
+    ${_.media.maxPhone`
+        height: 100%;
+        width: 100%;
+        max-width: 500px;
+    `}
+    ${_.media.minTablet`
+        grid-column-start: 3;
+        grid-column-end: 4;
+        grid-row-start: 1;
+        grid-row-end: 2;
+        justify-self: stretch;
+        align-self: stretch;
+    `}
 `;
 
-const Video = styled.div`
-    width: 700px;
-    height: 400px;
+const Video = styled.div`    
     background: var(--light);
+    
     align-self: center;
-    margin: 25px auto 100px;
+    
+    width: 90vw;
+    height: 50vw;
+    margin: 25px auto 50px;
+
+    ${_.media.minTablet`
+        width: 700px;
+        height: 400px;
+        margin: 25px auto 100px;
+        align-self: stretch;
+    `}
 `;
---
+++ src/helpers.js
@@ -0,0 +1,28 @@
+import styled, {css}   from 'styled-components';
+
+//Sizes for media templates
+const sizes = {
+    maxPhone: 768,
+    minTablet: 769,
+    maxTablet: 992,
+    minSmall: 993,
+    maxSmall: 1200,
+    minLarge: 1201
+}
+// Iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((acc, label) => {
+	acc[label] = (...args) => {
+        var limit = (label.indexOf('min') === -1) ? 'max' : 'min';
+        return css`
+            @media (${limit}-width: ${sizes[label] / 16}em) {
+                ${css(...args)}
+            }
+        }
+	    `;
+    }
+	return acc;
+}, {});
+
+export default {
+    media,
+}
